0b6d48429829d3b1e6b270f5819dd326aa2c041e
ValdrST <valdr.stiglitz@gmail.com>
2019-05-25 23:02:11 -0500

    Añadiendo script de creacion de usuarios y roles

---
+++ scripts/s-01-usuarios.sql
@@ -0,0 +1,12 @@
+--@Autor(es): Mario Garrido, Vicente Romero
+--@Fecha creación: 25/05/2019
+--@Descripción: Definicion de los usuarios invitado y admin y su asignacion de permisos
+connect sys as sysdba
+create user gr_proy_invitado identified by bravo123 quota 1024 m on users;
+create user gr_proy_admin identified by bravo123 quota 1024 m in users;
+create role rol_admin;
+create role rol_invitado;
+grant create session, create table, create view, create public synonym, create synonym, create sequence, create procedure, create trigger to rol_admin;
+grant create session to rol_invitado;
+grant rol_admin to gr_proy_admin;
+grant rol_invitado to gr_proy_invitado;

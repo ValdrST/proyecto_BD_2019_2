60aa51b8b24accef18e7f414b5ff85c8c8bb383c
Mgczacki <mg@czacki.com>
2019-05-26 23:43:37 -0500

    Update s-17-lob-scooter-anywhere.sql

--- scripts/s-17-lob-scooter-anywhere.sql
+++ scripts/s-17-lob-scooter-anywhere.sql
@@ -4,22 +4,13 @@
 connect gr_proy_admin/bravo123
 create or replace directory data_dir as '/tmp/bd';
 grant read,write on directory data_dir to gr_proy_invitado;
-create or replace procedure p_actualiza_imagen(a_nombre_archivo varchar(200)) is
+create or replace procedure p_inserta_imagen(a_reporte_id numeric(10,0), a_nombre_archivo varchar(200)) is
 v_bfile bfile;
 v_src_offset number := 1;
 v_dest_offset number := 1;
 v_dest_blob blob;
-v_src_length number;
-v_dest_length number;
 v_nombre_archivo varchar2(1000);
-cursor cur_imagen is
-select imagen_reporte_id,reporte_id,imagen
-from imagen_reporte;
-begin
-for r in cur_imagen loop
-v_src_offset := 1;
-v_dest_offset := 1;
-v_bfile := bfilename('DATA_DIR', a_nombre_archivo);
+v_bfile := bfilename('DATA_DIR', a_nombre_archivo);                                                                        
 if dbms_lob.fileexists(v_bfile) = 1 and not dbms_lob.isopen(v_bfile) = 1 then dbms_lob.open(
 v_bfile, dbms_lob.lob_readonly);
 else raise_application_error(-20001, 'El archivo '
@@ -27,28 +18,22 @@ else raise_application_error(-20001, 'El archivo '
 ||' no existe en el directorio DATA_DIR'
 || ' o el archivo esta abierto');
 end if;
+v_op_id numeric(10,0) := imagen_reporte_seq.nextval;
+insert into imagen_reporte (imagen_reporte_id, reporte_id, imagen)
+values(v_op_id,a_reporte_id,empty_blob());
 select imagen into v_dest_blob
 from imagen_reporte
-where reporte_id = r.reporte_id
+where imagen_reporte_id = v_op_id
 for update;
 dbms_lob.loadblobfromfile(
-dest_lob => v_dest_blob,
-src_bfile => v_bfile,
-amount => dbms_lob.getlength(v_bfile),
-dest_offset => v_dest_offset,
-src_offset => v_src_offset);
+  dest_lob => v_dest_blob,
+  src_bfile => v_bfile,
+  amount => dbms_lob.getlength(v_bfile),
+  dest_offset => v_dest_offset,
+  src_offset => v_src_offset);
 dbms_lob.close(v_bfile);
-v_src_length := dbms_lob.getlength(v_bfile);
-v_dest_length := dbms_lob.getlength(v_dest_blob);
-if v_src_length = v_dest_length then
-dbms_output.put_line('Escritura correcta, bytes escritos: '
-|| v_src_length);
-else raise_application_error(-20002, 'Error al escribir datos.\n'
-|| ' Se esperaba escribir '
-|| v_src_length
-|| ' Pero solo se escribio '
-|| v_dest_length);
-end if;
-end loop;
+update imagen_reporte
+set imagen = v_dest_blob
+where imagen_reporte_id = v_op_id;
 end;
 /

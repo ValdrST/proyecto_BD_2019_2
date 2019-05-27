b54e275226615a8a17a8e9d085dc1dfc9ebdc5fa
Mgczacki <mg@czacki.com>
2019-05-26 21:52:46 -0500

    Scripts version inicial. Necesitan probarse.

---
+++ scripts/s-07-sinonimos.sql
@@ -0,0 +1,25 @@
+--@Autores: Mario Garrido, Vicente Romero
+--@Fecha creación: 26/05/2019
+--@Descripción: Script encargado de generar sinónimos.
+connect gr_proy_admin/bravo123
+create or replace public synonym estado for estado;
+create or replace public synonym zona_aparato for zona_aparato;
+create or replace public synonym zona for zona;
+grant select on aparatos_disponibles to gr_proy_invitado;
+grant select on aparato_info_publica to gr_proy_invitado;
+grant select on aparatos_recarga to gr_proy_invitado;
+connect gr_proy_invitado/bravo123
+create or replace synonym aparatos_disponibles for gr_proy_admin.aparatos_disponibles;
+create or replace synonym aparato_info_publica for gr_proy_admin.aparato_info_publica;
+create or replace synonym aparatos_recarga for gr_proy_admin.aparatos_recarga;
+begin
+  set echo off
+  set pagesize 0
+  set linesize 1000
+  spool s-07-2-sinonimos.sql
+  select 'create public synonym XX_' || table_name || ' for ' || owner || '.' || table_name || ';'
+  from all_tables where owner='gr_proy_admin';
+  spool off
+  @s-07-2-sinonimos.sql
+end;
+/
---
+++ scripts/s-08-vistas.sql
@@ -0,0 +1,20 @@
+--@Autores: Mario Garrido, Vicente Romero
+--@Fecha creación: 26/05/2019
+--@Descripción: Script encargado de generar vistas
+connect gr_proy_admin/bravo123
+create or replace view aparatos_disponibles as--Vista de aparatos disponibles/ubi
+select a.aparato_id, u.latitud, u.longitud
+from aparato a
+join ubicacion u using(aparato_id)
+join estado e using(estado_id)
+where e.clave='ENSP';
+create or replace view aparato_info_publica as--Vista de informacion publica de aparatos
+select a.aparato_id, a.capacidad, a.numero_matriculo, a.porcentaje_carga, m.nombre as marca
+from aparato a
+join marca m using(marca_id);
+create or replace view aparatos_recarga as--Vista de ubicacion de aparatos que necesitan recarga/ubi
+select a.aparato_id, u.latitud, u.longitud
+from aparato a
+join ubicacion u using(aparato_id)
+join estado e using(estado_id)
+where e.clave='BATB';
